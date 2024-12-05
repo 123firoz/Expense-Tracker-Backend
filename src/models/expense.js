ed71927733872d70cce13926a40839245c0c2aa1
123firoz <firoz.ansari@merillife.com>
2024-12-05 09:51:55 +0530

    update

--- src/models/expense.js
+++ src/models/expense.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 // Schema and Models
 const expenseSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    amount:{type: Number, required: true},
+    name: {type: String, required: false},
+    amount:{type: Number, required: false},
     cratedAt:{type: Date,default: Date.now},
 });
 
